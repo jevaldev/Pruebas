4220e5a4e474911b16310097d6a826c0991d75a5
jevaldev <jesusvalvir0354@gmail.com>
2025-12-11 11:44:05 -0600

    feat(AdventJS2025): Add solution of problem #11

---
+++ AdventJS2025/Day#11.js
@@ -0,0 +1,26 @@
+function findUnsafeGifts(warehouse) {
+  let unsafeCount = 0;
+  const rows = warehouse.length;
+  if (rows === 0) return 0;
+  const cols = warehouse[0].length;
+
+  const isCamera = (r, c) =>
+    r >= 0 && r < rows && c >= 0 && c < cols && warehouse[r][c] === '#';
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (warehouse[r][c] !== '*') continue; 
+
+      const top = isCamera(r - 1, c);
+      const bottom = isCamera(r + 1, c);
+      const left = isCamera(r, c - 1);
+      const right = isCamera(r, c + 1);
+
+      if (!top && !bottom && !left && !right) {
+        unsafeCount++;
+      }
+    }
+  }
+
+  return unsafeCount;
+}
